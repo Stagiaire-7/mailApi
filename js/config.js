2421ff310d6951435acc91522aea43a03a63e499
Stagiaire-7 <71452689+Stagiaire-7@users.noreply.github.com>
2021-01-14 12:43:05 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_hTsHHoQXg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5j3j3e85m82197levb72ijvdm1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'eu-west-3_4bTDlDm1p', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7i4qr2grep891l47u7t4li1eis', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://zffq7nzac6.execute-api.us-east-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
