ec05572fc25a13749e23d4e7194eb16ee7a1c7a5
aboubakar-abd <abdoulbaguiaboubakar@gmail.com>
2021-01-14 11:42:04 +0100

    initial commit

---
+++ js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_hTsHHoQXg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5j3j3e85m82197levb72ijvdm1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://zffq7nzac6.execute-api.us-east-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+
+};
